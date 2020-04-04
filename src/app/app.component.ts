9ba24012ed0e243675f9bd7c2cab8f1397bb890a
Adilyam <adliyamt@gmail.com>
2020-04-04 22:32:28 +0600

    Added metaname and testing on css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'HR module';
+title = 'HR Module'
+  constructor(private meta: Meta) {
+  	  this.meta.addTag({ name: 'viewport', content: 'width=device-width,initial-scale=1,maximum-scale=1,user-scalable=no' })
+  }
 }
