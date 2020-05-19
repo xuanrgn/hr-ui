75b5ccb7f36a2f791e90fb6a321734ad484fae7e
Aktilek <tohakktl@gmail.com>
2020-05-19 06:34:11 +0600

    details

--- src/app/candidate-detail/candidate-detail.component.ts
+++ src/app/candidate-detail/candidate-detail.component.ts
@@ -4,6 +4,8 @@ import { Location } from '@angular/common';
 import { FormBuilder, FormGroup, Validators , ReactiveFormsModule} from "@angular/forms";
 import { Candidate } from "../candidate/candidate.model";
 import { Vacancy } from 'src/app/vacancy/vacancy.model';
+import { ActivatedRoute } from '@angular/router';
+import { CandidateService } from '../service/candidate.service';
 
 @Component({
   selector: 'app-candidate-detail',
@@ -14,17 +16,24 @@ import { Vacancy } from 'src/app/vacancy/vacancy.model';
 export class CandidateDetailComponent implements OnInit {
   form: FormGroup;
   error = null;
+  id = null;
   @Input() model: Candidate;
   @Input() vacancy: Vacancy;
 
   constructor(
-  	private _location: Location,
+    private route: ActivatedRoute,
+    private _location: Location,
+    private candidateService: CandidateService,
   	private formBuilder: FormBuilder) { 
   	this.createForm();
   }
 
   ngOnInit() {
+    this.id = this.route.snapshot.params["id"];
     // this.form.patchValue(this.model);
+    this.candidateService.get(this.id).subscribe((res) => {
+      
+    })
   }
 
   backClicked() {
