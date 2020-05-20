3597b8e1bb476e5336f61f51fe5a26ab72ada246
Adilyam <adliyamt@gmail.com>
2020-05-21 04:32:58 +0600

    3

--- server.js
+++ server.js
@@ -8,4 +8,4 @@ app.use(express.static(__dirname + '/dist'));
 app.get('/*', function(req,res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-app.listen(80);
+app.listen(process.env.PORT || 8080);
