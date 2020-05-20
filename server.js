23fea9cac91d1434de373645a79bc7a96bb608e9
Adilyam <adliyamt@gmail.com>
2020-05-21 04:45:53 +0600

    4

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/angular8-springboot-client'));
 
-app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.get('*', function (req, res) {
+    const index = path.join(__dirname, 'build', 'index.html');
+    res.sendFile(index);
 });
+
 app.listen(process.env.PORT || 8080);
